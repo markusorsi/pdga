dbdece618d10286ade302c2350b93f41170d4816
Markus Orsi <markusorsi@icloud.com>
2025-06-12 10:32:54 +0200

    updated default config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ Update the values below to change the behavior of the algorithm.
 CONFIG = {
     # Query settings
     'query': 'CC(C)CCCCC(=O)N[C@@H](CC[NH3+])C(=O)N[C@@H]([C@H](C)O)C(=O)N[C@@H](CC[NH3+])C(=O)N[C@@H](CCN8)C(=O)N[C@@H](CC[NH3+])C(=O)N[C@H](CC1=CC=CC=C1)C(=O)N[C@@H](CC(C)C)C(=O)N[C@@H](CC[NH3+])C(=O)N[C@@H](CC[NH3+])C(=O)N[C@@H]([C@H](C)O)C(=O)8',
-    'query_format': 'smiles',
+    'query_format': 'smiles', 
 
     # Population settings
     'pop_size': 50,
@@ -14,7 +14,7 @@ CONFIG = {
 
     # Genetic operators parameters
     'mutation_ratio': 0.8,
-    'cutoff': 0.9,
+    'cutoff': 0.5,
     'fitness_function': 'atompair',
     'selection_strategy': 'greedy',
     'crossover_method': 'single_point',
@@ -24,6 +24,6 @@ CONFIG = {
     'seed': 42,
 
     # Output settings
-    'run_id': 'ccr8',
+    'run_id': 'run',
     'maximize': False,
 }
